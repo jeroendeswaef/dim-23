4ae11a5df4318aa8e5936db684e33b69dca7f714
Jeroen De Swaef <j@recallq.com>
2025-03-16 16:24:04 +0100

    added restframework

--- navigator/urls.py
+++ navigator/urls.py
@@ -1,7 +1,23 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import serializers, viewsets, routers
 
 from . import views
+from .models import Item
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['update_date', 'content']
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+router = routers.DefaultRouter()
+router.register(r'items', ItemViewSet)
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("", include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
